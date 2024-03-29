58e439fac7b1c37ca57f687c69cd9c758e710f9c
Amey Tendulkar <tendulkaramey@gmail.com>
2024-03-29 11:26:51 +0530

    added 3 consumers for group, group period and normal periodic

--- track/routing.py
+++ track/routing.py
@@ -4,4 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('ws/test/<str:name>/', consumers.TestConsumer.as_asgi()),
+    path('ws/test-group/<str:name>/', consumers.TestConsumerGroupPeriodic.as_asgi()),
+    path('ws/test-basic/<str:name>/', consumers.TestConsumerBasic.as_asgi()),
 ]
